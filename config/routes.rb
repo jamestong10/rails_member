6b85e5ce1cc8af67bd134945c789ce5d27c20043
JamesTong <jameskai17@gmail.com>
2018-12-06 16:34:38 +0800

    Add fronted product page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  root 'products#index'
+  get '/products', to: 'products#index', as: 'products'
+  get '/product/:id', to: 'products#show', as: 'product'
+
   devise_for :users
   namespace :admin do
     resources :products
   end
+
 end
