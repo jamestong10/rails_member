3b854e00696fca5193ae4e00e53661f46a7c47e9
JamesTong <jameskai17@gmail.com>
2018-12-10 22:21:02 +0800

    Include factory bot methods

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -61,4 +61,5 @@ RSpec.configure do |config|
   # config.include Devise::TestHelpers, type: :controller
   # config.include Devise::TestHelpers, type: :view
   config.include Devise::Test::ControllerHelpers
+  config.include FactoryBot::Syntax::Methods
 end
