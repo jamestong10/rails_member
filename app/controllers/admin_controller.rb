9fcbc96b749158c5d997b2f917aa8b7980220bcf
JamesTong <jameskai17@gmail.com>
2018-12-14 00:58:27 +0800

    Fix error

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -5,12 +5,16 @@ class AdminController < ActionController::Base
   private
 
   def signed_in?
-    flash[:notice] = "Please Login"
-    redirect_to root_path unless user_signed_in?
+    unless user_signed_in?
+      flash[:notice] = "Please Login"
+      redirect_to root_path
+    end
   end
   
   def is_admin?
-    flash[:notice] = "You are not a admin"
-    redirect_to root_path unless current_user.has_role?(:admin)
+    unless current_user.has_role?(:admin)
+      flash[:notice] = "You are not a admin"
+      redirect_to root_path
+    end
   end
 end
