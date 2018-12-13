b2fcbec402b47278f15fa6bcdab7381479773edc
JamesTong <jameskai17@gmail.com>
2018-12-13 22:22:17 +0800

    Add user authroized

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,17 @@
 class UsersController < ApplicationController
+  rescue_from Pundit::NotAuthorizedError, with: :not_the_same_user
+
   def upgrade_primium
     @user = User.find(params[:id])
+    authorize @user
     @user.add_role(:premium)
+    redirect_to root_path
+  end
+
+  private
+
+  def not_the_same_user
+    flash[:notice] = "You can not upgrade to premium."
     redirect_to root_path
   end
 end
